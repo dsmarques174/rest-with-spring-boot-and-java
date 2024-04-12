a6d4ac4e54f06120b38e2994afaaf681bbe9d0a4
Daniel Marques <dsmarques@gmail.com>
2024-04-11 21:25:47 -0300

    Service and DB

--- rest-with-spring-boot-and-java/src/main/java/br/com/restwithspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/restwithspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.restwithspring.exceptions.ExceptionResponse;
+import br.com.restwithspring.exceptions.ResourceNotFoundException;
 import br.com.restwithspring.exceptions.UnsupportedMathOperationException;
 
 @ControllerAdvice
@@ -31,5 +32,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 }
